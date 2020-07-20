48c4c4c81cfd73ba46c9a191979b8f7df2c0cd65
Samuel Guevara <samsjamin@outlook.com>
2020-07-20 09:13:58 -0400

    initial commit

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
